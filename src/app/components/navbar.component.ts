8159933bf7259a19f2239baadbf345ced6f101cf
Parakum Dissanayake <parakumbbb@gmail.com>
2021-06-23 14:14:53 +0530

    Initial Commit

---
+++ src/app/components/navbar.component.ts
@@ -0,0 +1,11 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//     moduleId: module.id,
+//   selector: 'navbar',
+//   templateUrl: 'navbar.component.html',
+// //   styleUrls: ['./app.component.css']
+// })
+// export class NavbarComponent { name = "Angular";
+ 
+// }
